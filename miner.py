0fda2e5b060605500dc958078a16e1e5d1522fd1
Ross Fenning <ross@debian.national.core.bbc.co.uk>
2013-02-28 22:27:47 +0000

    Added some more methods

--- miner.py
+++ miner.py
@@ -10,11 +10,29 @@ Options:
 """
 from docopt import docopt
 import MySQLdb
+import itertools
+
+
+class Recipe:
+    def __init__(self, recipe_id, title):
+        self.recipe_id = recipe_id
+        self.title = title
+
+    def __str__(self):
+        return '<Recipe: {}>'.format(self.title)
+
 
 def fetch(cursor, query):
     cursor.execute(query)
     for row in cursor.fetchall():
         yield row
+
+
+def get_recipes(cursor):
+    query = 'SELECT id, title FROM recipes ORDER BY RAND()'
+    for recipe_id, title in itertools.islice(fetch(cursor, query), 10):
+        yield Recipe(recipe_id, title)
+
 
 if __name__ == '__main__':
    arguments = docopt(__doc__)
@@ -26,6 +44,7 @@ if __name__ == '__main__':
 
    cursor = connection.cursor() 
 
-   
+   for recipe in get_recipes(cursor):
+       print recipe
 
    connection.close()
